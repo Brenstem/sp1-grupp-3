da7ec7b58cd5350f6a4770390ae5625ebfc72f5b
Dane Nilsson <dane.nilsson@gmail.com>
2019-02-01 14:40:25 +0100

    Finished basic menu system
    Dane

---
+++ sp1-grupp-3/Assets/Assets/Scripts/Menu/MenuActions.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour {
+
+    [SerializeField] bool selectOnInput;
+    [SerializeField] EventSystem canvasEventSystem;
+    [SerializeField] GameObject selectOnStart;
+
+    private bool buttonSelected = false;
+
+    // checks for player movement and selects menu buttons according to movement
+    private void Update()
+    {
+        if (selectOnInput)
+        {
+            if (Input.GetAxis("Vertical") != 0 && !buttonSelected)
+            {
+                canvasEventSystem.SetSelectedGameObject(selectOnStart);
+                buttonSelected = true;
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+
+
+    // Quits game/stops playmode depending on build
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+
+    // Scene loading
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
