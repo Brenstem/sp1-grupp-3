25504ac8da2dc9f0545251e2293a09f0db2396ee
MongoLloydSuperstar <Marcus.Svedbergh@users.noreply.github.com>
2019-02-04 17:03:30 +0100

    Modified path script
    Marcus

---
+++ sp1-grupp-3/Assets/Assets/Platform/Platform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Platform : MonoBehaviour {
+    public PathCreator pathCreator;
+    public float speed = 5;
+    float distanceTravelled;
+
+	void Update () {
+        distanceTravelled += speed * Time.deltaTime;
+
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+
+        Quaternion rotationOffsetY = Quaternion.Euler(Vector3.up * 90); //Makes 2D sprites face camera
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled) * rotationOffsetY;
+	}
+}
