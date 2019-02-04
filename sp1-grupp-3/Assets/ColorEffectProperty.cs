357b21939482212dd9a258b4743bb1504ae1b8db
Clismy <47212901+Clismy@users.noreply.github.com>
2019-02-04 16:16:05 +0100

    Color Effect Works for now
    Christos

--- sp1-grupp-3/Assets/ColorEffectProperty.cs
+++ sp1-grupp-3/Assets/ColorEffectProperty.cs
@@ -2,38 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColourEffect : MonoBehaviour
+[ExecuteInEditMode]
+public class ColorEffectProperty : MonoBehaviour
 {
     public Color fullColor;
     public Color noColor;
     public float speed;
-    GameObject child1, child2;
-    bool enableEffect = false;
 
-	void Start ()
+    bool enable = false;
+
+    void Update()
     {
-        //child1 = transform.GetChild(0).gameObject;
-        //child2 = transform.GetChild(1).gameObject;
-	}
-	
-	void Update ()
-    {
-		//if(Input.GetKeyDown(KeyCode.F))
-  //      {
-  //          enableEffect = !enableEffect;
-  //      }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            enable = !enable;
+        }
+        
+        var objecs = GameObject.FindObjectsOfType<ColourEffect>();
 
-  //      if(enableEffect == true)
-  //      {
-  //          EnableColor();
-  //      }
-  //      else
-  //      {
-  //          DisableColor();
-  //      }
-	}
+        for (int i = 0; i < objecs.Length; i++)
+        {
+            objecs[i].fullColor = fullColor;
+            objecs[i].noColor = noColor;
+            objecs[i].speed = speed;
 
-    public void EnableColor()
+            if (enable == true)
+            {
+                EnableColor(objecs[i].transform.GetChild(0).gameObject, objecs[i].transform.GetChild(1).gameObject);
+            }
+            else
+            {
+                DisableColor(objecs[i].transform.GetChild(0).gameObject, objecs[i].transform.GetChild(1).gameObject);
+            }
+        }
+    }
+
+    public void EnableColor(GameObject child1, GameObject child2)
     {
         Color trueColor = child1.GetComponent<SpriteRenderer>().color;
         trueColor.a = Mathf.MoveTowards(trueColor.a, fullColor.a, Time.deltaTime * speed);
@@ -43,7 +47,7 @@ public class ColourEffect : MonoBehaviour
         trueNoColor.a = Mathf.MoveTowards(trueNoColor.a, noColor.a, Time.deltaTime * speed);
         child2.GetComponent<SpriteRenderer>().color = trueNoColor;
     }
-    public void DisableColor()
+    public void DisableColor(GameObject child1, GameObject child2)
     {
         Color trueColor = child1.GetComponent<SpriteRenderer>().color;
         trueColor.a = Mathf.MoveTowards(trueColor.a, noColor.a, Time.deltaTime * speed);
