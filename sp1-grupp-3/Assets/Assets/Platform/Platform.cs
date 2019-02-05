e83db77fdbbe16aed3b0c6551a896b1d9359d96e
MongoLloydSuperstar <Marcus.Svedbergh@users.noreply.github.com>
2019-02-05 10:37:35 +0100

    Worked on platforms
    Marcus

--- sp1-grupp-3/Assets/Assets/Platform/Platform.cs
+++ sp1-grupp-3/Assets/Assets/Platform/Platform.cs
@@ -6,14 +6,21 @@ using PathCreation;
 public class Platform : MonoBehaviour {
     public PathCreator pathCreator;
     public float speed = 5;
+    public bool reversePath;
     float distanceTravelled;
 
-	void Update () {
+	void FixedUpdate ()
+    {
+        MoveAlongPath();
+    }
+
+    private void MoveAlongPath()
+    {
         distanceTravelled += speed * Time.deltaTime;
 
         transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
 
         Quaternion rotationOffsetY = Quaternion.Euler(Vector3.up * 90); //Makes 2D sprites face camera
         transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled) * rotationOffsetY;
-	}
+    }    
 }
