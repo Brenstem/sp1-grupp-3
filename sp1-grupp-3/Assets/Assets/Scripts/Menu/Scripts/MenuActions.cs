f55cd134f4eb51f341d4ed44fab6a17f2752485a
Dane Nilsson <dane.nilsson@gmail.com>
2019-02-05 15:15:57 +0100

    Fixed menu!

--- sp1-grupp-3/Assets/Assets/Scripts/Menu/Scripts/MenuActions.cs
+++ sp1-grupp-3/Assets/Assets/Scripts/Menu/Scripts/MenuActions.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class MenuActions : MonoBehaviour {
-
+public class MenuActions : MonoBehaviour, IPointerEnterHandler, ISelectHandler
+{
+    // Serialized variables
     [SerializeField] GameObject selectOnStart;
 
+    // Private variables
     private EventSystem canvasEventSystem;
     private bool buttonSelected = false;
+    private bool keyDown = false;
     
+    // Sound references
     [FMODUnity.EventRef]
     public string onSelectSound;
     FMOD.Studio.EventInstance onSelect;
@@ -19,38 +23,44 @@ public class MenuActions : MonoBehaviour {
     public string onStartSound;
     FMOD.Studio.EventInstance onStart;
 
+    [FMODUnity.EventRef]
+    public string onExitSound;
+    FMOD.Studio.EventInstance onExit;
+
+    // Reference fetching
     private void Start()
     {
         canvasEventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
     }
 
-    // checks for keyboard/gamepad input and selects menu buttons according to movement
+    // Checks for keyboard/gamepad input and switches to keyboard/gamepad control
     private void Update()
     {
-
         if (Input.GetAxis("Vertical") != 0 && !buttonSelected)
         {
             canvasEventSystem.SetSelectedGameObject(selectOnStart);
             buttonSelected = true;
         }
 
-        if (canvasEventSystem.IsPointerOverGameObject())
+        // Switches back to mouse control if the player clicks
+        if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1))
         {
             buttonSelected = false;
-            canvasEventSystem.SetSelectedGameObject(null);
         }
-
-        if (this.gameObject == canvasEventSystem.currentSelectedGameObject)
-        {
-            PlaySound(sounds.Select);
-        }
-        Debug.Log(canvasEventSystem.currentSelectedGameObject);
-
     }
 
-    private void OnDisable()
+    // Checks for mouse hovering over buttons
+    public void OnPointerEnter(PointerEventData eventData)
     {
+        PlaySound(sounds.Select);
         buttonSelected = false;
+        canvasEventSystem.SetSelectedGameObject(null);
+    }
+
+    // Checks for when new objects are selected with keyboard/gamepad input to play sound
+    public void OnSelect(BaseEventData eventData)
+    {
+        PlaySound(sounds.Select);
     }
 
 
@@ -66,16 +76,17 @@ public class MenuActions : MonoBehaviour {
 
 
     // Scene loading
-    public void LoadSceneByName(string sceneName)
+    public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
-    public void LoadSceneByIndex(int sceneIndex)
+    public void LoadScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
     }
 
+    // Sound playing
     public void PlaySound(GetEnum sound)
     {
         switch (sound.state)
@@ -89,6 +100,7 @@ public class MenuActions : MonoBehaviour {
                 onSelect = FMODUnity.RuntimeManager.CreateInstance(onSelectSound);
                 break;
             default:
+                onExit = FMODUnity.RuntimeManager.CreateInstance(onExitSound);
                 break;
         }
 
@@ -109,6 +121,7 @@ public class MenuActions : MonoBehaviour {
                 onSelect = FMODUnity.RuntimeManager.CreateInstance(onSelectSound);
                 break;
             default:
+                onExit = FMODUnity.RuntimeManager.CreateInstance(onExitSound);
                 break;
         }
 
