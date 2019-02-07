faea65640cb98959144f6cf1a2bdb997468ec96f
Flytnyllet <47013995+Flytnyllet@users.noreply.github.com>
2019-02-07 16:38:03 +0100

    Ny Movement
    För att designers ska börja utan problem
    
    Christoffer

---
+++ sp1-grupp-3/Assets/Assets/PlayerMovementAssets/Scripts/Player/Christoffer/AutoMoveCreate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMoveCreate : PlayerMovementChristoffer
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        targetVelocity = Vector2.left;
+    }
+}
---
+++ sp1-grupp-3/Assets/Assets/PlayerMovementAssets/Scripts/Player/Christoffer/PlayerControllerChristoffer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerChristoffer : PlayerMovementChristoffer
+{
+    public float maxSpeed = 7;
+    public float jumpForce = 7;
+    public float acceleration = 1;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private float currentSpeed;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded) {
+            velocity.y = jumpForce;
+        }
+        else if (Input.GetButtonUp("Jump")) {
+            if (velocity.y > 0)
+                velocity.y = velocity.y * 0.5f;
+        }
+
+        bool flipSprite = spriteRenderer.flipX ? (move.x < -0.01f) : (move.x > 0.01f);
+
+        if (flipSprite) {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x / maxSpeed));
+
+        targetVelocity = move * maxSpeed;
+    }
+
+    // Behöver implementeras som gravity/sensitivity
+    public void UpdateSpeed()
+    {
+        if (Input.GetAxisRaw("Horizontal") == 1) {
+            currentSpeed += acceleration;
+        }
+        else if (Input.GetAxisRaw("Horizontal") == -1) {
+            currentSpeed -= acceleration;
+        }
+        else if (currentSpeed > 0){
+            currentSpeed -= acceleration;
+        }
+        else if (currentSpeed < 0) {
+            currentSpeed += acceleration;
+        }
+    }
+}
