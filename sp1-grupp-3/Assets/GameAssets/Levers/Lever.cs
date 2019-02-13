092ad8d649597d0ec7ce36bf379701a13fa955ec
MongoLloydSuperstar <Marcus.Svedbergh@users.noreply.github.com>
2019-02-13 16:32:40 +0100

    Started working on levers

---
+++ sp1-grupp-3/Assets/GameAssets/Levers/Lever.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public GameObject[] affectedObjectArray;
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (CheckCollision(collision) && Input.GetKeyDown("Fire1")) {
+            OnPullLever();
+        }
+    }
+
+    private bool CheckCollision(Collision2D collision)
+    {
+        bool isCollided = collision.transform.CompareTag("Player");
+
+        return isCollided;
+    }
+
+    private void OnPullLever()
+    {
+        foreach (GameObject gameObject in affectedObjectArray) {
+            if (gameObject.GetComponent<ObjectLeverAction>() as ObjectLeverAction != null) {
+                ObjectLeverAction action = gameObject.GetComponent<ObjectLeverAction>();
+
+                action.OnActivatedByLever();
+                action.enabled = false;
+
+            }
+            else {
+                Debug.Log("No ObjectLeverAction script attached!");
+            }
+        }
+    }
+
+    public void ResetLever()
+    {
+
+    }
+}
---
+++ sp1-grupp-3/Assets/GameAssets/Levers/ObjectLeverAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectLeverAction : MonoBehaviour
+{
+    public enum ObjectAction { DoNothing, Move, ActivateComponent, SetVariables }
+    public ObjectAction objectAction;
+
+    private bool actionPerformed = false;
+    
+    public void OnActivatedByLever()
+    {
+        actionPerformed = true;
+
+        switch (objectAction) {            
+            case ObjectAction.Move:
+
+                break;
+            case ObjectAction.ActivateComponent:
+
+                break;
+            case ObjectAction.SetVariables:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void MoveObject()
+    {
+        Vector3.MoveTowards();
+    }
+
+    
+}
