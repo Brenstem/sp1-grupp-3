43314697df011bbc8ac018f8e220625eea887c88
Flytnyllet <47013995+Flytnyllet@users.noreply.github.com>
2019-02-13 16:39:54 +0100

    Particle Added
    Christoffer

--- sp1-grupp-3/Assets/GameAssets/Player/Scripts/GroundCheck.cs
+++ sp1-grupp-3/Assets/GameAssets/Player/Scripts/GroundCheck.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour
 {
     public bool isGrounded = false;
+    public bool checkGrounded = false;
     public Vector3 groundCollPosition;
     public Vector3 groundCollSize;
     public LayerMask collideWithLayer;
+
+    [SerializeField]
+    GameObject dustEffect;
 
 	void Update ()
     {
         var groundHits = new RaycastHit2D[10];
         int hitcount = Physics2D.BoxCast(transform.position + groundCollPosition, groundCollSize, 0f, Vector2.zero, new ContactFilter2D { useLayerMask = true, layerMask = collideWithLayer }, groundHits);
+        checkGrounded = isGrounded;
         isGrounded = hitcount > 0;
+        if (checkGrounded == false && isGrounded == true) {
+            Instantiate(dustEffect, transform.position + groundCollPosition, transform.rotation);
+        }
 	}
 
     void OnDrawGizmos()
