6846002116dca41946d3f7277fb53c358f773ad4
Dane Nilsson <dane.nilsson@gmail.com>
2019-02-13 11:32:08 +0100

    Okay maybe saved the day

--- sp1-grupp-3/Assets/GameAssets/Player/Scripts/GroundCheck.cs
+++ sp1-grupp-3/Assets/GameAssets/Player/Scripts/GroundCheck.cs
@@ -9,16 +9,11 @@ public class GroundCheck : MonoBehaviour
     public Vector3 groundCollSize;
     public LayerMask collideWithLayer;
 
-    [SerializeField]
-    GameObject dustEffect;
-
 	void Update ()
     {
         var groundHits = new RaycastHit2D[10];
         int hitcount = Physics2D.BoxCast(transform.position + groundCollPosition, groundCollSize, 0f, Vector2.zero, new ContactFilter2D { useLayerMask = true, layerMask = collideWithLayer }, groundHits);
         isGrounded = hitcount > 0;
-        if (isGrounded)
-            Instantiate(dustEffect, transform.position + groundCollPosition, dustEffect.transform.rotation);
 	}
 
     void OnDrawGizmos()
