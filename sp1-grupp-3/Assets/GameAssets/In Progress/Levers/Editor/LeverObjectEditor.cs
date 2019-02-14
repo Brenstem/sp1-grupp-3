a28a4a34ce42feede5851485423519499fed9f8e
MongoLloydSuperstar <Marcus.Svedbergh@users.noreply.github.com>
2019-02-14 14:58:30 +0100

    Levers Base Functionality

---
+++ sp1-grupp-3/Assets/GameAssets/In Progress/Levers/Editor/LeverObjectEditor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LeverObject)), CanEditMultipleObjects]
+public class LeverObjectEditor : Editor
+{
+    public SerializedProperty actionProperty;
+
+    private LeverObject leverObject;
+
+    private void OnEnable()
+    {
+        actionProperty = serializedObject.FindProperty("objectAction");
+        leverObject = (LeverObject)target;
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        actionProperty.enumValueIndex = EditorGUILayout.Popup(actionProperty.enumValueIndex, actionProperty.enumDisplayNames);
+
+        LeverObject.ObjectAction objectAction = (LeverObject.ObjectAction)actionProperty.enumValueIndex;
+
+        switch (objectAction) {
+            case LeverObject.ObjectAction.DoNothing:
+                break;
+            case LeverObject.ObjectAction.Move:
+                leverObject.moveTo = EditorGUILayout.Vector2Field("Move To", leverObject.moveTo);
+                break;
+            case LeverObject.ObjectAction.ActivatePhysics:
+                break;
+            case LeverObject.ObjectAction.ActivatePlatform:
+                leverObject.platfrom = (Platform)EditorGUILayout.ObjectField("Platform", leverObject.platfrom, typeof(Platform), false);
+                break;
+            default:
+                break;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+}
---
+++ sp1-grupp-3/Assets/GameAssets/In Progress/Levers/LeverObject.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverObject : MonoBehaviour
+{
+    #region Fields
+
+    public enum ObjectAction { DoNothing, Move, ActivatePhysics, ActivatePlatform }
+    [SerializeField] public ObjectAction objectAction;
+    public Vector2 moveTo;
+    public Platform platfrom;
+
+
+    private Vector2 currentPosition;
+    private float moveIncrement;
+
+    private bool actionPerformed = false;
+
+    #endregion
+
+    #region Set/Get Functions
+
+    public bool ActionPerformed { get => actionPerformed; private set => actionPerformed = value; }
+
+    #endregion
+
+    #region Internal Methods
+
+    private void Update()
+    {
+        MoveObject();
+    }
+
+    private void MoveObject()
+    {
+        if (actionPerformed && objectAction == ObjectAction.Move) {
+            transform.position = Vector2.MoveTowards(currentPosition, moveTo, moveIncrement);
+
+            moveIncrement += Time.deltaTime * 5;
+        }
+    }
+
+    private void ActivatePhysics()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        if (rb != null) {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void OnActivatedByLever()
+    {
+        actionPerformed = true;
+
+        switch (objectAction) {
+            case ObjectAction.Move:
+                currentPosition = transform.position;
+                moveIncrement = 0.0f;
+                break;
+
+            case ObjectAction.ActivatePhysics:
+                ActivatePhysics();
+                break;
+
+            case ObjectAction.ActivatePlatform:
+                platfrom.move = true;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    #endregion
+
+}
