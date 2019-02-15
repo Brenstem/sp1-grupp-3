1b3811b313739d3427e71da30842b99d9561ae7f
Dane Nilsson <dane.nilsson@gmail.com>
2019-02-15 11:55:59 +0100

    Started work on cutscene scripts

---
+++ sp1-grupp-3/Assets/GameAssets/In Progress/Cutscenes/Scripts/TriggerCutscene.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerCutscene : MonoBehaviour
+{
+    public GameObject[] images;
+
+    private Animator[] imageAnimators;
+    private Timer animationTimer;
+    private bool animationPlaying;
+
+    private void Start()
+    {
+        animationTimer = new Timer();
+        
+        for (int i = 0; i < images.Length; i++)
+        {
+            imageAnimators[i] = images[i].GetComponent<Animator>();
+        }
+    }
+
+    private void Update()
+    {
+        animationTimer.UpdateTimer();
+
+        if (animationTimer.TimerFinished)
+        {
+            animationPlaying = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.CompareTag("Player"))
+        {
+            PlayCutscene();
+        }
+    }
+
+    private void PlayCutscene()
+    {
+        for (int i = 0; i < images.Length; i++)
+        {
+            animationPlaying = true;
+            images[i].SetActive(true);
+            animationTimer.StartTimer(imageAnimators[i].GetCurrentAnimatorStateInfo(0).length);
+        }
+    }
+}
