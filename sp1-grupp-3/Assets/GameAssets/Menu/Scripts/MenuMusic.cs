c1af7c8e5ad056d9dbc6f1c657c8293a1a31a003
alliebeans <47175901+alliebeans@users.noreply.github.com>
2019-02-18 11:40:15 +0100

    Menu music -change

---
+++ sp1-grupp-3/Assets/GameAssets/Menu/Scripts/MenuMusic.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour
+{
+    private float menuTimer;
+    private float onTransition;
+
+    [FMODUnity.EventRef]
+    public string menuMusicSound;
+    FMOD.Studio.EventInstance menuMusic;
+
+    void Start()
+    {
+        PlaySound(sounds.menuMusic);
+        menuTimer = 0f;
+        onTransition = 0f;
+    }
+
+    public void PlaySound(sounds sound)
+    {
+        menuMusic = FMODUnity.RuntimeManager.CreateInstance(menuMusicSound);
+        menuMusic.start();
+        menuMusic.release();
+    }
+
+    public void Update()
+    {
+        MenuTransition();
+    }
+
+    private void MenuTransition()
+    {
+        menuTimer += Time.deltaTime;
+
+        if (menuTimer > 298.0f)
+        {
+            onTransition = 1.0f;
+            menuMusic.setParameterValue("Menu Timer", onTransition);
+        }
+    }
+}
