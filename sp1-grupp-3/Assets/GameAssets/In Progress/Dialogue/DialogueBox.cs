d7017dfac9b239e087c2997372db8e374d836845
MongoLloydSuperstar <Marcus.Svedbergh@users.noreply.github.com>
2019-02-19 10:12:24 +0100

    Removed Fungus Added own system

---
+++ sp1-grupp-3/Assets/GameAssets/In Progress/Dialogue/DialogueBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBox : MonoBehaviour
+{
+    public GameObject speaker;
+    public Vector2 boxOffset;
+    public Vector2 padding;
+
+    private Canvas canvas;
+    private RectTransform rectTransform;
+
+    private void Start()
+    {
+        canvas = GetComponentInParent<Canvas>();
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        Vector2 childSpriteSize = GetComponentInChildren<RectTransform>().sizeDelta;
+
+        rectTransform.sizeDelta = childSpriteSize + padding;
+
+        transform.position = speaker.transform.position + (Vector3)boxOffset;
+    }
+}
