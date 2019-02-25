2c8605749cc824520b988f5b81f3bbc932902c13
MongoLloydSuperstar <Marcus.Svedbergh@users.noreply.github.com>
2019-02-25 13:06:12 +0100

    Nästan klar dialog

--- sp1-grupp-3/Assets/GameAssets/In Progress/Dialogue/DialogueBox.cs
+++ sp1-grupp-3/Assets/GameAssets/In Progress/Dialogue/DialogueBox.cs
@@ -2,27 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class DialogueBox : MonoBehaviour
 {
     public GameObject speaker;
     public Vector2 boxOffset;
-    public Vector2 padding;
+    public Vector2 triggerOffset;
 
-    private Canvas canvas;
-    private RectTransform rectTransform;
-
-    private void Start()
-    {
-        canvas = GetComponentInParent<Canvas>();
-        rectTransform = GetComponent<RectTransform>();
-    }
 
     private void Update()
     {
-        Vector2 childSpriteSize = GetComponentInChildren<RectTransform>().sizeDelta;
+        PositionUpdate();
+    }
+        
 
-        rectTransform.sizeDelta = childSpriteSize + padding;
+    private void PositionUpdate()
+    {
+        Collider2D childCollider = GetComponentInChildren<BoxCollider2D>();
 
         transform.position = speaker.transform.position + (Vector3)boxOffset;
-    }
+
+        childCollider.transform.position = transform.position;
+        childCollider.offset = -boxOffset + triggerOffset;
+    }    
+       
 }
---
+++ sp1-grupp-3/Assets/GameAssets/In Progress/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    Image image;
+
+    private void Start()
+    {
+        image = transform.parent.GetComponent<Image>();
+        image.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        image.enabled = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        image.enabled = false;
+    }
+
+    private void EnableImage(bool state)
+    {
+    }
+}
