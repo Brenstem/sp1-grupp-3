027e036d9baf1f98bf816073984ef83d65f67cb3
Dane Nilsson <dane.nilsson@gmail.com>
2019-02-26 10:56:38 +0100

    Added color changing

--- sp1-grupp-3/Assets/GameAssets/Cutscenes/Scripts/TriggerColorChange.cs
+++ sp1-grupp-3/Assets/GameAssets/Cutscenes/Scripts/TriggerColorChange.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class TriggerColorChange : MonoBehaviour
 {
     [SerializeField] string playerTag;
-    [SerializeField] bool colorEnabled;
+    //[SerializeField] bool colorEnabled;
 
     private void OnTriggerEnter2D(Collider2D hitInfo)
     {
         if (hitInfo.CompareTag(playerTag))
         {
-            
+            Camera.main.GetComponent<ColorChangerEffect>().ActivateEffect();
         }
     }
 }
