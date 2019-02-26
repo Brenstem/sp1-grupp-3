23e96257caecb9bbeed167b848fd12eb7acca42b
alliebeans <47175901+alliebeans@users.noreply.github.com>
2019-02-26 10:31:46 +0100

    animationsound, scripts

---
+++ sp1-grupp-3/Assets/GameAssets/In Progress/Animation Sound Implementation/Scripts/SoundEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEvent : MonoBehaviour
+{
+    //Loop instance
+    private string path;
+    FMOD.Studio.EventInstance loopSound;
+
+    void PlayOneShot(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+    }
+
+    void PlayLoopable(string path)
+    {
+        loopSound = FMODUnity.RuntimeManager.CreateInstance(path);
+        loopSound.start();
+    }
+
+    void OnDestroyLoop(string path)
+    {
+        loopSound.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        loopSound.release();
+    }
+}
