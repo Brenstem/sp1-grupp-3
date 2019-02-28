7b80659be296bc32bc6291f95cc2e88086653532
alliebeans <47175901+alliebeans@users.noreply.github.com>
2019-02-28 14:45:49 +0100

    Added sound implementation

---
+++ sp1-grupp-3/Assets/GameAssets/In Progress/Animation Sound Implementation/Scripts/AmbientPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientPlayer : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string path;
+    FMOD.Studio.EventInstance sound;
+
+    private bool hasPlayed;
+    private FMOD.Studio.EventInstance lastSound;
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        sound = FMODUnity.RuntimeManager.CreateInstance(path);
+
+        if (!hasPlayed)
+        {
+            sound.start();
+            hasPlayed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Stop(sound);
+    }
+
+    private void Stop(FMOD.Studio.EventInstance soundToStop)
+    {
+        soundToStop.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        soundToStop.release();
+        hasPlayed = false;
+    }
+}
---
+++ sp1-grupp-3/Assets/GameAssets/In Progress/Animation Sound Implementation/Scripts/MusicJunkyard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicJunkyard : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string path;
+    FMOD.Studio.EventInstance sound;
+
+    private void OnTriggerEnter2D()
+    {
+        sound = FMODUnity.RuntimeManager.CreateInstance(path);
+        sound.start();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Stop(sound);   
+    }
+
+    private void Stop(FMOD.Studio.EventInstance soundToStop)
+    {
+        soundToStop.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        soundToStop.release();
+    }
+}
