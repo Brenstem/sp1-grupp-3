000267ad4f72c0e139546ec8203b6ad6a97a5346
Flytnyllet <47013995+Flytnyllet@users.noreply.github.com>
2019-03-11 11:09:14 +0100

    Fixed walk particle effect

--- sp1-grupp-3/Assets/DustEffectPlay.cs
+++ sp1-grupp-3/Assets/DustEffectPlay.cs
@@ -6,19 +6,26 @@ public class DustEffectPlay : MonoBehaviour
 {
     ParticleSystem particle;
     GroundCheck grounded;
+    public float emmisionRate;
+    ParticleSystem.EmissionModule emission;
 
     void Start()
     {
         particle = GetComponent<ParticleSystem>();
-        grounded = GetComponent<GroundCheck>();
+        grounded = GetComponentInParent<GroundCheck>();
+        emission = particle.emission;
+
     }
 
     void Update()
     {
-        if (grounded.HasLanded() == true)
-        {
-            Debug.Log("meme");
-            particle.Play();
+
+        if (grounded.isGrounded) {
+            emission.rateOverDistance = emmisionRate;
+
+        }
+        else if (!grounded.isGrounded) {
+            emission.rateOverDistance = 0f;
         }
     }
 }
