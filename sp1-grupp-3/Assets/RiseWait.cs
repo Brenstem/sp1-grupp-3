8e8a3c1a4aa58fab217660fd505b236ffd8b8bfd
Flytnyllet <47013995+Flytnyllet@users.noreply.github.com>
2019-03-11 14:25:08 +0100

    Player_RiseWait
    Christos
    Christoffer

--- sp1-grupp-3/Assets/RiseWait.cs
+++ sp1-grupp-3/Assets/RiseWait.cs
@@ -6,16 +6,29 @@ public class RiseWait : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateinfo, int layerindex)
     {
-        Debug.Log(animator.gameObject.GetComponent<PlayerMovement>());
-        //animator.gameObject.GetComponent<PlayerMovement>().enabled = false;
-        //animator.gameObject.GetComponent<PlayerJump>().enabled = false;
-
+        //Debug.Log(animator.gameObject.GetComponent<PlayerMovement>());
+        //GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = false;
+        //GameObject.Find("Player").GetComponent<PlayerJump>().enabled = false;
+        if (stateinfo.IsName("Player_Rise"))
+        {
+            GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = false;
+            GameObject.Find("Player").GetComponent<PlayerJump>().enabled = false;
+        }
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateinfo, int layerindex)
     {
+        if (stateinfo.IsName("Player_Rise"))
+        {
+            GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
+            GameObject.Find("Player").GetComponent<PlayerJump>().enabled = true;
+        }
         //Debug.Log(animator.gameObject.GetComponent<PlayerMovement>());
-        //animator.gameObject.GetComponent<PlayerMovement>().enabled = true;
-        //animator.gameObject.GetComponent<PlayerJump>().enabled = true;
+        //GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
+        //GameObject.Find("Player").GetComponent<PlayerJump>().enabled = true;
+    }
+    public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
     }
 }
