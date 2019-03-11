9ca05f225ffcfd739e0a8148ac0567f7c0363194
Dane Nilsson <dane.nilsson@gmail.com>
2019-03-11 11:10:12 +0100

    Moved door object script

--- sp1-grupp-3/Assets/GameAssets/Dialogue/Scripts/DialogueBox.cs
+++ sp1-grupp-3/Assets/GameAssets/Dialogue/Scripts/DialogueBox.cs
@@ -24,7 +24,6 @@ public class DialogueBox : MonoBehaviour
             transform.position = speaker.transform.position + (Vector3)boxOffset;
         }
 
-
         childCollider.transform.position = transform.position;
         childCollider.offset = -boxOffset + triggerOffset;
         childCollider.size = triggerSize;
