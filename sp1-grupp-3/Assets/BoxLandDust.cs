d954c9877c46efb194546566be7710667f07e0e8
Flytnyllet <47013995+Flytnyllet@users.noreply.github.com>
2019-03-11 12:53:18 +0100

    box sound

---
+++ sp1-grupp-3/Assets/BoxLandDust.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxLandDust : MonoBehaviour
+{
+    ParticleSystem particle;
+    SoundEvent sound;
+    GroundCheck gc;
+    bool previousGrounded = false;
+
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+        gc = GetComponentInParent<GroundCheck>();
+        sound = GetComponent<SoundEvent>();
+    }
+
+    void Update()
+    {
+        if (gc.isGrounded) {
+            if (previousGrounded == false) {
+                particle.Play();
+                sound.PlayOneShot("event:/Object/Box/box_1stland");
+                previousGrounded = gc.isGrounded;
+            }
+        }
+        previousGrounded = gc.isGrounded;
+    }
+}
