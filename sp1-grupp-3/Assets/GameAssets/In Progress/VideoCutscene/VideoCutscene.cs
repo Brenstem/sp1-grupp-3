fe90f3663ffec8b6f6998c1ec9fc43e064be29ce
Marcus Svedbergh <ezzzet@live.com>
2019-03-11 15:30:20 +0100

    Created video player

---
+++ sp1-grupp-3/Assets/GameAssets/In Progress/VideoCutscene/VideoCutscene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoCutscene : MonoBehaviour
+{
+    public bool videoPlaying = false;
+
+    private VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && !videoPlayer.isPlaying) {
+            videoPlayer.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && videoPlayer.isPlaying) {
+            videoPlayer.Stop();
+        }
+
+
+        if (true) {
+
+        }
+
+        videoPlayer.loopPointReached += StopVideo;        
+    }
+
+    private void StopVideo(UnityEngine.Video.VideoPlayer vp)
+    {
+        vp.Stop();
+    }
+}
