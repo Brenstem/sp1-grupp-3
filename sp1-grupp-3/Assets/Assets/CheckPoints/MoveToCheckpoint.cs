144584521a2c47553f38fe8cfd57965251ad463d
Flytnyllet <47013995+Flytnyllet@users.noreply.github.com>
2019-03-12 15:58:35 +0100

    asd

--- sp1-grupp-3/Assets/Assets/CheckPoints/MoveToCheckpoint.cs
+++ sp1-grupp-3/Assets/Assets/CheckPoints/MoveToCheckpoint.cs
@@ -3,26 +3,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class MoveToTrigger : MonoBehaviour
+public class RespawnTrigger : MonoBehaviour
 {
     public event Action<GameObject> OnMove;
     protected void TriggerMove(GameObject obj) { OnMove(obj); }
 }
 
-public class MoveToCheckpoint : MonoBehaviour {
+public class RespawnObject : MonoBehaviour
+{
 
     [SerializeField]
-    MoveToTrigger MoveTrigger;
+    RespawnTrigger RespawnTrigger;
+    PlayerMovement playerMove;
+    PlayerJump playerJump;
+    public float timer;
+    bool respawn = false;
 
     public Transform checkpoint;
-    
-	void Start () {
-        MoveTrigger.OnMove += Move;
-	}
+
+    void Start()
+    {
+        RespawnTrigger = GetComponent<RespawnTrigger>();
+        playerMove = GetComponent<PlayerMovement>();
+        playerJump = GetComponent<PlayerJump>();
+        RespawnTrigger.OnMove += Move;
+    }
+
+    private void Update()
+    {
+        if (respawn)
+            Move(gameObject);
+    }
 
     private void Move(GameObject obj)
     {
-        transform.position = checkpoint.position;
+        respawn = true;
+        playerMove.enabled = false;
+        playerJump.enabled = false;
+
+        if (timer <= 0) {
+            obj.transform.position = checkpoint.position;
+            timer = 2;
+            respawn = false;
+            playerMove.enabled = true;
+            playerJump.enabled = true;
+        }
+        else
+            timer -= Time.deltaTime;
     }
 }
